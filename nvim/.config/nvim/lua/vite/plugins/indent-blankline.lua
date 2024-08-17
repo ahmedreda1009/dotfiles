a097d826573630f8ea6a94aac075297da6f8a615
Ahmed Reda <ahmedreda@Ahmeds-MacBook-Pro.local>
2024-08-17 12:26:15 +0300

    Modified: indentation line

--- nvim/.config/nvim/lua/vite/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/vite/plugins/indent-blankline.lua
@@ -1,27 +1,64 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	event = "VeryLazy",
-	opts = {
-		indent = {
-			char = "│",
-			tab_char = "│",
-		},
-		scope = { show_start = true, show_end = true },
-		exclude = {
-			filetypes = {
-				"help",
-				"alpha",
-				"dashboard",
-				"neo-tree",
-				"Trouble",
-				"trouble",
-				"lazy",
-				"mason",
-				"notify",
-				"toggleterm",
-				"lazyterm",
-			},
-		},
-	},
-	main = "ibl",
+  "lukas-reineke/indent-blankline.nvim",
+  event = "VeryLazy",
+  opts = {
+    indent = {
+      -- char = "│",
+      -- tab_char = "│",
+      char = "▍",
+      tab_char = "▍",
+      -- char = "▌",
+      -- tab_char = "▌",
+    },
+    whitespace = {
+      remove_blankline_trail = false,
+    },
+    scope = { show_start = true, show_end = true },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "nvim-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
+  },
+  main = "ibl",
 }
+
+
+-- Default: `▎` ~
+--
+-- Alternatives: ~
+--  • left aligned solid
+--    • `▏`
+--    • `▎` (default)
+--    • `▍`
+--    • `▌`
+--    • `▋`
+--    • `▊`
+--    • `▉`
+--    • `█`
+--  • center aligned solid
+--    • `│`
+--    • `┃`
+--  • right aligned solid
+--    • `▕`
+--    • `▐`
+--  • center aligned dashed
+--    • `╎`
+--    • `╏`
+--    • `┆`
+--    • `┇`
+--    • `┊`
+--    • `┋`
+--  • center aligned double
+--    • `║`
