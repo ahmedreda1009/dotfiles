c93f298089e0b819ec37c074126366267f1283c7
Ahmed Reda <ahmedreda@Ahmeds-MacBook-Pro.local>
2024-08-18 17:05:45 +0300

    Modified: git signs in starship and nvim

--- nvim/.config/nvim/lua/vite/plugins/git-signs.lua
+++ nvim/.config/nvim/lua/vite/plugins/git-signs.lua
@@ -5,15 +5,16 @@ return {
     require("gitsigns").setup({
       signs = {
         -- add = { text = "┃" },
-        add = { text = "" },
-        delete = { text = "" },
-        topdelete = { text = "‾" },
+        add          = { text = "" },
+        change       = { text = "✎" },
+        delete       = { text = "✘" },
+        topdelete    = { text = "‾" },
         changedelete = { text = "~" },
-        untracked = { text = "┆" },
+        untracked    = { text = "…" },
       },
       signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-      numhl = false,  -- Toggle with `:Gitsigns toggle_numhl`
-      linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+      numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
       word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
       watch_gitdir = {
         follow_files = true,
@@ -34,7 +35,7 @@ return {
       -- },
       sign_priority = 6,
       update_debounce = 100,
-      status_formatter = nil, -- Use default
+      status_formatter = nil,  -- Use default
       max_file_length = 40000, -- Disable if file is longer than this (in lines)
       preview_config = {
         -- Options passed to nvim_open_win
