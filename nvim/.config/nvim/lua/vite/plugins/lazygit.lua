e5f7370fb235760d9e757895562e714093e1478d
Ahmed Reda <ahmedreda@Ahmeds-MacBook-Pro.local>
2024-08-23 15:40:55 +0300

    Modified: close mini.files before opening lazygit

--- nvim/.config/nvim/lua/vite/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/vite/plugins/lazygit.lua
@@ -1,19 +1,24 @@
+local function lazygit_mapping()
+  require('mini.files').close()
+  vim.api.nvim_command("LazyGit")
+end
+
 return {
-	"kdheepak/lazygit.nvim",
-	cmd = {
-		"LazyGit",
-		"LazyGitConfig",
-		"LazyGitCurrentFile",
-		"LazyGitFilter",
-		"LazyGitFilterCurrentFile",
-	},
-	-- optional for floating window border decoration
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	-- setting the keybinding for LazyGit with 'keys' is recommended in
-	-- order to load the plugin when the command is run for the first time
-	keys = {
-		{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
-	},
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {
+    { "<leader>gg", lazygit_mapping, desc = "Open Lazy Git" },
+  },
 }
