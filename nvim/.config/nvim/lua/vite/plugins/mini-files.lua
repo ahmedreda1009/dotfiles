39f902a536fa5bea2eb271cc41b52b0f0c03d36f
Ahmed Reda <ahmedreda@Ahmeds-MacBook-Pro.local>
2024-08-23 15:04:57 +0300

    Modified: prevent mini.files from going outside the entry dir

--- nvim/.config/nvim/lua/vite/plugins/mini-files.lua
+++ nvim/.config/nvim/lua/vite/plugins/mini-files.lua
@@ -5,6 +5,10 @@ return {
     local MiniFiles = require('mini.files')
     MiniFiles.setup({
       sync_current_file = true,
+      -- mappings = {
+      --   go_in = 'L',
+      --   go_in_plus = 'l',
+      -- },
     })
 
     -- Function to toggle mini.files
@@ -33,5 +37,43 @@ return {
     vim.cmd([[
       highlight MiniFilesTitleFocused guifg=#fab387
     ]])
+
+
+    ---- prevent the go out fn to go out of the entry directory
+    local anchor_dir = ""
+    local is_mini_files_explorer_open_event_triggered = false
+
+    local function get_curr_dir()
+      local curr_path = MiniFiles.get_fs_entry().path
+      local curr_dir = vim.fs.dirname(curr_path)
+      return curr_dir
+    end
+    -- get the entry directory path per session
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesExplorerOpen",
+      callback = function()
+        if not is_mini_files_explorer_open_event_triggered then
+          anchor_dir = get_curr_dir()
+          is_mini_files_explorer_open_event_triggered = true
+        end
+      end
+    })
+    -- Custom function to override go_out()
+    local function custom_go_out()
+      local current_dir = get_curr_dir()
+      if current_dir == anchor_dir then
+        print("Cannot navigate outside the initial directory.")
+      else
+        MiniFiles.go_out()
+      end
+    end
+    -- Map the custom go_out function
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'MiniFilesBufferCreate',
+      callback = function(args)
+        local buf_id = args.data.buf_id
+        vim.keymap.set('n', 'h', custom_go_out, { buffer = buf_id })
+      end,
+    })
   end,
 }
