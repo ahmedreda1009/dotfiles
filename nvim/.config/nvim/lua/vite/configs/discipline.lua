892e4788de389a027aaff0cc266de6ff9bef6b9a
Ahmed Reda <ahmedreda@Ahmeds-MacBook-Pro.local>
2024-10-02 20:46:49 +0300

    Modified: tmux theme

--- nvim/.config/nvim/lua/vite/configs/discipline.lua
+++ nvim/.config/nvim/lua/vite/configs/discipline.lua
@@ -12,12 +12,12 @@ function M.cowboy()
       if vim.v.count > 0 then
         count = 0
       end
-      if count >= 20 then
+      if count >= 30 then
         ok, id = pcall(vim.notify, "Hold it Cowboy!", vim.log.levels.WARN, {
           icon = "🤠",
           replace = id,
           keep = function()
-            return count >= 20
+            return count >= 30
           end,
         })
         if not ok then
