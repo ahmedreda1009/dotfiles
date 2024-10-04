0abf4660b04d805bd686ad894c86c3ffe3f137a0
Ahmed Reda <ahmedreda@Ahmeds-MacBook-Pro.local>
2024-10-04 18:00:10 +0300

    Modified: auto save events

--- nvim/.config/nvim/lua/vite/plugins/autosave.lua
+++ nvim/.config/nvim/lua/vite/plugins/autosave.lua
@@ -27,11 +27,18 @@ return {
       trigger_events = {          -- See :h events
         immediate_save = {
           "BufLeave",
-          "FocusLost"
+          "FocusLost",
+          "BufWinLeave",
+          "BufUnload",
+          "BufDelete",
+          "BufHidden",
+          "BufWritePost",
+          "BufWritePre",
+          "BufWrite",
         }, -- vim events that trigger an immediate save
         defer_save = {
           "InsertLeave",
-          -- "TextChanged"
+          "TextChanged"
         },                                       -- vim events that trigger a deferred save (saves after `debounce_delay`)
         cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
       },
